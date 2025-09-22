59e6e73bb1b2628c3d7f3b5b9a9a3d4a6471cfd7
AT200210 <amaythakur09@gmail.com>
2025-09-22 10:45:15 +0530

    Changes in new branch

--- Test/test.py
+++ Test/test.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
 
 a=np.array([10,12,13,45,17])
 print(a)
 print(f"Mean for the n-dimensional array: {a.mean()}")
+
+data=sns.load_dataset('tips')
+print(data.head(10))
